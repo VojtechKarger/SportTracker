70694018cf8b0eb431e41d611d18697674fbbb25
Vojtěch Karger <vojtech.karger@kiwi.com>
2024-08-21 12:25:51 +0200

    removed uitests target

--- SportTracker/AppDelegate.swift
+++ SportTracker/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISceneDelegate {
     var appCoordinator: AppCoordinator?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         return true
     }
     
